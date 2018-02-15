cc721029ac46e01314e185900dd626aced3fa53e
Colin Chung <wrynthian@gmail.com>
2018-02-15 10:26:47 -0500

    Add files via upload

--- chicken/js/main.js
+++ chicken/js/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1080, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1080, 720, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 var player;
 var goal;
 var goalPosition;
